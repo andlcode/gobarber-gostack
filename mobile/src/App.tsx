9376c11f221bc7d462b31042459336803e5616ef
Andre Luiz D C <and969696@outlook.com>
2021-02-22 13:34:30 -0300

    problem in render app

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -1,12 +1,15 @@
 import 'react-native-gesture-handler';
+
 import React from 'react';
-import {StatusBar, View} from 'react-native';
+import { View, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
 import Routes from './routes';
-import {NavigationContainer} from '@react-navigation/native';
+
 const App: React.FC = () => (
   <NavigationContainer>
     <StatusBar barStyle="light-content" backgroundColor="#312e38" />
-    <View style={{flex: 1, backgroundColor: '#312e38'}}>
+    <View style={{ flex: 1, backgroundColor: '#312e38' }}>
       <Routes />
     </View>
   </NavigationContainer>
